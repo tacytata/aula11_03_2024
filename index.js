5eee59c637079774875592b0c0b1e48a0e1dae6d
tacytata <tacyegi30@gmail.com>
2024-03-12 14:51:38 -0300

    abrir arquivo com fs

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
+import fs from 'fs';
+
 import chalk from 'chalk';
 
-console.log(chalk.blue('Olá mundo'));
+// console.log(chalk.blue('Olá mundo'));
 
-console.log(chalk.blue.bgWhite.bold("SENAI"));
+// console.log(chalk.blue.bgWhite.bold("SENAI"));
 
-console.log(chalk.blue('Curso',' de',' Node.JS'));
+// console.log(chalk.blue('Curso',' de',' Node.JS'));
 
-console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
+// console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
 
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+// console.log(`
+// CPU: ${chalk.red('90%')}
+// RAM: ${chalk.green('40%')}
+// DISK: ${chalk.yellow('70%')}
+// `);
+
+//2 parte - Tratamento de erros
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code,"não há arquivo no diretório"));
+}
+
+//1 parte - escrever uma function que traga arquivos .md (traz o conteudo do arquivo, lê ele)
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = "utf-8";
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
+        if(erro){
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    });
+}
+//pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
