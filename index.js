b2d20fa1b52cf91c97e25aeaf965fbc64ea6ce44
tacytata <tacyegi30@gmail.com>
2024-03-14 15:27:01 -0300

    subindo arquivos regex

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ import fs from 'fs';
 
 import chalk from 'chalk';
 
+const regex='\[[^[\]]*?\]\(https?:\/\/[^\s?#.]*[^\s]*\)';
+
+const textoTeste= 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks(texto){
+    const regex= /[[^[\]]*?\]\(https?:\/\/[^\s?#.]*[^\s]*\)/gm;
+
+    const capturas=texto.match(regex);
+
+    console.log(capturas);
+}
+
+extraiLinks(textoTeste);
+
 // console.log(chalk.blue('Olá mundo'));
 
 // console.log(chalk.blue.bgWhite.bold("SENAI"));
@@ -17,7 +31,7 @@ import chalk from 'chalk';
 // `);
 
 
- function trataErro(erro){
+  function trataErro(erro){
      throw new Error(chalk.red(erro.code,"não há arquivo no diretório"));
  }
 //--------------aula sobre promessas----
@@ -35,20 +49,20 @@ import chalk from 'chalk';
 
 //2 parte - outra forma de solucionar as promessas
 
-async function pegaArquivo(caminhoDoArquivo){
-    try{
-        const encoding="utf-8";
-        const texto=await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.cyan(texto));
+// async function pegaArquivo(caminhoDoArquivo){
+//     try{
+//         const encoding="utf-8";
+//         const texto=await fs.promises.readFile(caminhoDoArquivo, encoding);
+//         console.log(chalk.cyan(texto));
         
-    } catch (erro){
-        trataErro(erro);
-    } finally{
-        console.log(chalk.magenta('Operação concluída'));
-    }
+//     } catch (erro){
+//         trataErro(erro);
+//     } finally{
+//         console.log(chalk.magenta('Operação concluída'));
+//     }
     
 
-}
+// }
 
 //pegaArquivo('./arquivos/');
-pegaArquivo('./arquivos/texto.md');
+//pegaArquivo('./arquivos/texto.md');
