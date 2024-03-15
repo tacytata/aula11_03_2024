1f47fb6b78328c3d3a77ffc4d96c351a8eb05f18
tacytata <tacyegi30@gmail.com>
2024-03-15 15:17:08 -0300

    subir modelo resultados

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const regex='\[[^[\]]*?\]\(https?:\/\/[^\s?#.]*[^\s]*\)';
 const textoTeste= 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
 
 function extraiLinks(texto){
-    const regex= /[[^[\]]*?\]\(https?:\/\/[^\s?#.]*[^\s]*\)/gm;
+    const regex= /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm;
 
-    const capturas=texto.match(regex);
+    //const capturas=texto.match(regex);
+    //const capturas=texto.exec(regex);
+    const capturas=[...texto.matchAll(regex)];
 
-    console.log(capturas);
+    const resultados=capturas.map(captura=>({[captura[1]]:captura[2]}));
+
+    console.log(resultados);
+    
 }
 
 extraiLinks(textoTeste);
